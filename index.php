15b1517106363e8ec0204a86e3a1b4b5b7df649a
Ruby Boyle <rboyle@csumb.edu>
2018-11-01 05:57:56 +0000

    more

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
-<html><body>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Ruby's Web Portfolio</title>
+    </head>
+    <body>
 <?php
 // A simple web site in Cloud9 that runs through Apache
 // Press the 'Run' button on the top to start the web server,
 // then click the URL that is emitted to the Output tab of the console
-
-echo "Ruby Boyle's CST 352 Site";
+//<!--comment-->
+echo "<h1>Ruby Boyle's CST 352 Site</h1>";
 
 ?>
-</body>
+    </body>
 </html>
